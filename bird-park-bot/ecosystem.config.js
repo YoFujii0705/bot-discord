4f93e4d638c26f7447d10da583eaabf00dd4eac7
YoFujii0705 <yo.fujii.hitohitsuji@gmail.com>
2025-07-22 21:07:47 +0900

    Update ecosystem.config.js

--- bird-park-bot/ecosystem.config.js
+++ bird-park-bot/ecosystem.config.js
@@ -1,1 +1,24 @@
-
+module.exports = {
+  apps: [{
+    name: 'bird-bot',
+    script: './src/bot.js',
+    instances: 1,
+    exec_mode: 'fork',
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true,
+    autorestart: true,
+    max_restarts: 10,
+    min_uptime: '10s',
+    restart_delay: 4000
+  }]
+};
